5b1969629d50ea90f7094807cc6d11b16d37a1dd
taecongs <bks0927w@gmail.com>
2022-08-09 23:32:42 +0900

    study: 2022-08-09

---
+++ nodejs/2022-08-09-review/2022-08-09최종본.sql
@@ -0,0 +1,10 @@
+use sesac;
+
+CREATE TABLE membership (
+      id VARCHAR(20) not null,
+      pw VARCHAR(20) not null,
+      name VARCHAR(10) not null,
+      email VARCHAR(30) not null
+);
+
+DESC membership;
