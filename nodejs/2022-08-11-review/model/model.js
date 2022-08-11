90014c36e920eb55e39d7fbe0f8c2498bbe3d6e8
taecongs <bks0927w@gmail.com>
2022-08-11 22:17:13 +0900

    study:2022-08-11 시퀄라이즈 변환

--- nodejs/2022-08-11-review/model/model.js
+++ nodejs/2022-08-11-review/model/model.js
@@ -3,9 +3,11 @@
 - 어떤 데이터가 들어가는 정의
 */
 
+// sequlize를 불러온다.
 const Sequelize = require('sequelize');
 
 
+// config.json에 development로 정의 된 내용들을 가지고 온다.
 const config = require('../config/config.json')["development"];
 
 
@@ -24,9 +26,11 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 
+// 시퀄라이즈 문법을 사용할 때는 항상 모델(테이블 구조)이 존재해야 한다. 
 db.Members = require('./Members')(sequelize, Sequelize);
 
 
+// db는 키 : 값 형태의 {'sequelize' : sequelize, 'Sequelize' : Sequelize} 모듈로 내보낸다.
 module.exports = db;
 
 
