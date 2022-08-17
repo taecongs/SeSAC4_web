acfbcf93566456c3e7bed3fe198e48c3268ca62a
taecongs <bks0927w@gmail.com>
2022-08-17 23:22:59 +0900

    study: 2022-08-17 프로젝트 준비

---
+++ 2022-08-17/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+const bodyParser = require('body-parser');
+
+
+app.set('view engine', 'ejs');
+app.use('/', express.static('public'));
+app.use(express.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.get('/membership', (req,res) => {
+    res.render('membership.ejs');
+});
+
+app.get('/login', (req,res) => {
+    res.render('login.ejs');
+});
+
+
+app.listen(port, () => {
+    console.log('Server port : ', port);
+});
