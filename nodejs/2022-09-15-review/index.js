2a657cf646a6c6f2e760a296e629da60333bb0c6
taecongs <bks0927w@gmail.com>
2022-09-15 20:31:53 +0900

    study:2022-09-15 복습

--- nodejs/2022-09-15-review/index.js
+++ nodejs/2022-09-15-review/index.js
@@ -3,73 +3,83 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+let path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function(req, res){
-    // console.log('client');
-    res.sendFile(__dirname + '/chart.html');
+    res.sendFile(__dirname + '/index.html');
 });
-
-
 
 
 let list = {};
 
-// io -> 클라이언트와의 모든 연결을 가지고 있는 친구
-// socket -> 클라이언트 한 명. socket.id 클라이언트를 구분하는 식별자
+// io => 클라이언트와의 모든 연결
+// socket => 클라이언트 한 명
 io.on('connection', function(socket){
-    console.log('connected : ', socket.id);
+    console.log('Connected : ', socket.id);
 
+
+    // ○○○ 님이 입장하셨습니다.
     socket.on('info2', function(data){
-        // list[socket.id] = data.nickname는 prompt에 입력되는 아이디
         list[socket.id] = data.nickname;
+
+        // prompt 에서 입력한 아이디 === list[socket.id] === data.nickname
+        console.log('입력한 아이디 : ', list[socket.id]);
+
+        //서버에 접속되어 있는 모든 클라이언트에게 메시지 전송
         io.emit('welcome', data.nickname + '님이 입장하셨습니다.');
+
+        // 서버에 접속되어 있는 모든 클라이언트에게 Select 목록 전송
         io.emit('list', list);
     });
 
-    // socket.emit('info', socket.id);
-    
+
 
     socket.on('send', function(data){
-        // data = {msg : ~~~, to : ''};
-        console.log('client message : ', data.msg);
-        
-        // 구분을 위해 
+        // data : {msg : msg, to : nick};
+        console.log('입력한 메시지 : ', data.msg);
+
         data['is_dm'] = false;
-        
-        // list[socket.id] = data.nickname을 찾겠다.
+
         data['nickname'] = list[socket.id];
 
+        // data.to : {to : nick(document.getElementById('nick-list').value)}
         if(data.to == '전체'){
+            // 글 작성 후 전송 버튼 누르면 작성 된다.
             io.emit('newMessage', data);
         } else{
             data['is_dm'] = true;
 
-            // 선택한 닉네님을 가진 고유 식별자를 선택하는 방법?
-            // find(key => list[key] === data.to)로 사용해도 된다.
+            // Object.keys() - 키 배열 반환
             let socketID = Object.keys(list).find((key) => {return list[key] === data.to});
             console.log(socketID);
             console.log(data);
 
             // io.to는 특정한 사람에게만 보내는 방법
             io.to(socketID).emit('newMessage', data);
-            
+
             // 특정한 사람에게 보낸 메시지가 나에게 보이게 하는 방법
             socket.emit('newMessage', data);
         }
-
     });
 
 
     socket.on('disconnect', function(){
-        io.emit('welcome', list[socket.id] + '님이 퇴장하셨습니다.' );
+        io.emit('welcome', list[socket.id] + '님이 퇴장하셨습니다.');
 
         // 나간 사람을 서버에서 삭제하는 방법
         delete list[socket.id];
         io.emit('list', list);
-
     })
-});
+})
 
 
-http.listen(8002, function(){
-    console.log('Server Port : ', 8002);
+
+
+
+
+
+
+http.listen(7001, function(){
+    console.log('Server Port : ', 7001);
 });
