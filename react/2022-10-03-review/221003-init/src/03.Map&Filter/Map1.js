ba3e8c9fa299978efcab821810604a85a476fd8f
taecongs <bks0927w@gmail.com>
2022-10-04 16:23:02 +0900

    study: 2022-10-03~04 React&Sass 복습

--- react/2022-10-03-review/221003-init/src/03.Map&Filter/Map1.js
+++ react/2022-10-03-review/221003-init/src/03.Map&Filter/Map1.js
@@ -4,7 +4,9 @@ import React from 'react';
 const Map1 = () => {
     const baseball = ['LG트윈스', 'SSG 랜더스', '키움 히어로즈', 'KT 위즈'];
 
-    const bsTeam = baseball.map((baseball, team) => <li key={team}>{baseball}</li>);
+    // map() 첫번째 인자(arrayResult)는 리스트를 순서대로 돌면서 나오는 값
+    // map() 두번째 인자(arrayInt)는 방금 나온 값의 인덱스
+    const bsTeam = baseball.map((arrayResult, arrayInt) => <li key={arrayInt}>{arrayResult}</li>);
     return(
         <div>
             <h1>Map 복습</h1>
