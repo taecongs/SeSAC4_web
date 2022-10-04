c56a56de2dc0b75c7b8eff2bf087ad901d2bbf55
taecongs <bks0927w@gmail.com>
2022-10-04 16:19:43 +0900

    study: 2022-10-03~04 React&Sass 복습

--- react/2022-10-03-review/221003-init/src/03.Map&Filter/Map1.js
+++ react/2022-10-03-review/221003-init/src/03.Map&Filter/Map1.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 
 const Map1 = () => {
+    const baseball = ['LG트윈스', 'SSG 랜더스', '키움 히어로즈', 'KT 위즈'];
+
+    const bsTeam = baseball.map((baseball, team) => <li key={team}>{baseball}</li>);
     return(
         <div>
             <h1>Map 복습</h1>
+
+            <ul>
+                {bsTeam}
+            </ul>
         </div>
     );
 }
